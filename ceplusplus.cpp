82d6e07de7257506f6ed34185a211d0b46b7dfd2
Kasia <k.weglarz@op.pl>
2019-12-14 09:08:59 +0100

    ceplusplus

--- ceplusplus.cpp
+++ ceplusplus.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main (){
+
+string s1= "wlazl kotek na plotek i mruga";
+for ( auto c : s1 ) cout << c << " ";
+for ( auto& c : s1 ) c = ( c=='w' ) ? 'W' : c; 
+for ( int i=0; i < s1.length(); ++i ) cout << s1[i] << " "; 
+
+
+}
+
